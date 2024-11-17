b8eeb17dca1d7a993f69a4e582172dd6a87c6948
Tihomir Marinov <Tihomir-Bewerbungs-Email21@gmx.de>
2024-11-17 15:37:02 +0100

    Using factorials

---
+++ factorial.cpp
@@ -0,0 +1,33 @@
+ #include <iostream>
+
+using namespace std; 
+
+int factorial( int factorialNumber){
+    if (factorialNumber > 1) {
+        return factorialNumber * factorial(factorialNumber - 1 );
+    }
+    else {
+        return 1;
+    }
+}
+
+int main() {
+
+int number;
+
+cout << "Enter your numbere" << '\n';
+cin >> number;
+
+if (number < 0) {
+    cout << "Enter an valid Number" << '\n';
+} 
+else {
+    int result = factorial(number);
+    cout << "The result is " << result << '\n';
+}
+
+
+    
+};   
+
+
